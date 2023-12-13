41b37095ffae4584c8e3e13e79b36c5acaaf24e4
ling-ling <lingcy.work@gmail.com>
2023-12-12 23:59:30 -0500

    feat: add new product page

---
+++ pages/product/product_a.tsx
@@ -0,0 +1,14 @@
+import { Button } from "components/Button/Button"
+ 
+ 
+ const ProductA = ()=>{
+    return(
+        <div>
+           <h1>This is Product A</h1> 
+
+            <Button href=''>CTA</Button>
+        </div>
+    )
+}
+
+export default ProductA
---
+++ pages/product/product_a_new.tsx
@@ -0,0 +1,14 @@
+import { Button } from "components/Button/Button"
+ 
+ 
+ const ProductANew = ()=>{
+    return(
+        <div>
+           <h1>This is Product A Page NEW!!!!!!</h1> 
+
+            <Button href=''>CTA</Button>
+        </div>
+    )
+}
+
+export default ProductANew
